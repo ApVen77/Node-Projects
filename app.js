3e917cd771c85d2e326ef4c504618b22d1e2b947
April Long <april11dance@gmail.com>
2021-03-09 12:33:02 -0700

    all the files

--- app.js
+++ app.js
@@ -12,15 +12,20 @@
 
 
 
-//for Jade 
+//for Jade https://naltatis.github.io/jade-syntax-docs/
+// https://html2jade.org/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
 
 
 //to make POST request 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+// to use bootstrap from public folder
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 
@@ -36,7 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-//A post request for NODEMAILER 
+//A post request for NODEMAILER https://nodemailer.com/about/
 
 
 
