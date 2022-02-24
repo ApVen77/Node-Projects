e3c0a47b7534ffa68c92c99ea32e62cb9b3338b7
April Long <april11dance@gmail.com>
2022-02-24 11:17:06 -0700

    wiped clean for class

---
+++ server.js
@@ -0,0 +1,32 @@
+// dependencies include 4- express, path, body-parser, nodemailer
+
+
+
+//create express app and port
+
+
+
+//Set the view engine as jade
+
+
+//middleware to make POST request
+
+
+// to use bootstrap from public folder
+
+
+//Routes
+//for jade(html) files in the views folder
+//res.render Used in Express to view and send HTML to the client
+
+
+
+
+
+
+//A post request for NODEMAILER https://nodemailer.com/about/
+
+
+
+
+// Start the server to listen for request
